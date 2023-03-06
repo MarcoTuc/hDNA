1cedb1ecc8281343d99a9bef794496ab4bd31094
MarcoTuc <marco.tuccio95@gmail.com>
2023-03-06 18:36:44 +0100

    bounded

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 from scipy.optimize import dual_annealing
 
-EXPNAME = '120theta'
+EXPNAME = '120theta3'
 
 # Import experimental data from Hertel 
 expdata = pd.read_csv('./data/herteldata.csv', names=['sequences', 'experimental'])
